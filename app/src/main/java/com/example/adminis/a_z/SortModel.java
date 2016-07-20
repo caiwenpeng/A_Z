459cfa2674ab7cbbfbab38cf94401cfef5e4de34
cwp <1146751867@qq.com>
2016-07-20 22:21:11 +0800

    A_Z

---
+++ app/src/main/java/com/example/adminis/a_z/SortModel.java
@@ -0,0 +1,26 @@
+package com.example.adminis.a_z;
+
+/**
+ * Created by Adminis on 2016/7/20.
+ */
+public class SortModel {
+
+    private String name;
+    private String getSortLetters;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSortLetters() {
+        return getSortLetters;
+    }
+
+    public void setSortLetters(String getSortLetters) {
+        this.getSortLetters = getSortLetters;
+    }
+}
